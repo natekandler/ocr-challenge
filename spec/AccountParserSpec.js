8ab7c3b38634c0320b22dc6cead7edd2c7d86d2f
natekandler <natekandler@gmail.com>
2019-02-17 15:57:39 -0800

    allows to concat each set of three in an array by index

--- spec/AccountParserSpec.js
+++ spec/AccountParserSpec.js
@@ -5,8 +5,8 @@ describe("Account Parser", () => {
   it("chunks a line into sets of three", () => {
     const parser = new AccountParser();
     const line = "abcdefghijklmnopqrstuvwxyz1"
-    const expected = parser.chunkLine(line)
-    console.log(expected)
+    const expected = parser.chunkLineIntoThrees(line)
+    
     expect(expected[0]).toBe("abc");
     expect(expected[1]).toBe("def");
     expect(expected[2]).toBe("ghi");
@@ -17,4 +17,22 @@ describe("Account Parser", () => {
     expect(expected[7]).toBe("vwx");
     expect(expected[8]).toBe("yz1");
   });
+
+  it("should stack sets by index", () => {
+    const parser = new AccountParser();
+    const line = "abcdefghijklmnopqrstuvwxyz1"
+    const firstPass = parser.chunkLineIntoThrees(line)
+    const expected = parser.chunkLineIntoThrees(line, firstPass)
+    
+    expect(expected[0]).toBe("abcabc");
+    expect(expected[1]).toBe("defdef");
+    expect(expected[2]).toBe("ghighi");
+    expect(expected[3]).toBe("jkljkl");
+    expect(expected[4]).toBe("mnomno");
+    expect(expected[5]).toBe("pqrpqr");
+    expect(expected[6]).toBe("stustu");
+    expect(expected[7]).toBe("vwxvwx");
+    expect(expected[8]).toBe("yz1yz1");
+  });
+  
 });
--- src/AccountParser.js
+++ src/AccountParser.js
@@ -1,14 +1,16 @@
 class AccountParser {
 
-  chunkLine(line) {
+  chunkLineIntoThrees(line, arr=[]) {
     // TODO: look into a way to dynamically generate these pairs
     // handles the specification for lines of 27 but is brittle
     const pairs = [[0,3], [3,6], [6,9], [9,12],[12,15], [15,18], [18,21], [21,24], [24,27]]
 
-    return pairs.map(item => {
-      return line.slice(item[0], item[1])
+    pairs.forEach((item, index) => {
+      arr[index] = line.slice(item[0], item[1]).concat(arr[index] || "")
     })
+    return arr;
   }
+
 }
 
 module.exports = AccountParser;
