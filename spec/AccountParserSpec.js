5a8b4139ae854c5dc06ca9076608ce66c2437c7d
natekandler <natekandler@gmail.com>
2019-02-17 15:43:40 -0800

    adds method to chunk line into segments of 3

--- spec/AccountParserSpec.js
+++ spec/AccountParserSpec.js
@@ -1,8 +1,20 @@
 var AccountParser = require('../src/AccountParser.js');
 
-describe("Account Parser", function() {
-  it("returns true", function() {
+describe("Account Parser", () => {
+
+  it("chunks a line into sets of three", () => {
     const parser = new AccountParser();
-    expect(parser.chunkLine()).toBe(true);
+    const line = "abcdefghijklmnopqrstuvwxyz1"
+    const expected = parser.chunkLine(line)
+    console.log(expected)
+    expect(expected[0]).toBe("abc");
+    expect(expected[1]).toBe("def");
+    expect(expected[2]).toBe("ghi");
+    expect(expected[3]).toBe("jkl");
+    expect(expected[4]).toBe("mno");
+    expect(expected[5]).toBe("pqr");
+    expect(expected[6]).toBe("stu");
+    expect(expected[7]).toBe("vwx");
+    expect(expected[8]).toBe("yz1");
   });
 });
--- src/AccountParser.js
+++ src/AccountParser.js
@@ -1,7 +1,13 @@
 class AccountParser {
 
-  chunkLine() {
-    return true;
+  chunkLine(line) {
+    // TODO: look into a way to dynamically generate these pairs
+    // handles the specification for lines of 27 but is brittle
+    const pairs = [[0,3], [3,6], [6,9], [9,12],[12,15], [15,18], [18,21], [21,24], [24,27]]
+
+    return pairs.map(item => {
+      return line.slice(item[0], item[1])
+    })
   }
 }
 
