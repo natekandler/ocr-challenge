f04e4974656580be929fded1bd5e1cf8d31e0bd9
natekandler <natekandler@gmail.com>
2019-02-17 16:33:36 -0800

    adds file reader and account.txt file

---
+++ src/FileReader.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('Accounts.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  for await (const line of rl) {
+    console.log(`Line from file: ${line}`);
+  }
+}
+
+processLineByLine();
