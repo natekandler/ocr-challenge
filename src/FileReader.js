695e5041b56abe89297548d9188d655cc2545629
natekandler <natekandler@gmail.com>
2019-02-17 16:50:10 -0800

    add comments for direction I was heading in file reader

--- src/FileReader.js
+++ src/FileReader.js
@@ -5,7 +5,15 @@ const rl = readline.createInterface({
   input: fs.createReadStream('Accounts.txt'),
   crlfDelay: Infinity
 });
+// TODO: turn this into a class, add line arrays as attributes
+
 let counter = 1
+// TODO: possibly use numbered line objects that reset when counter resets
+// build up four lines and then send them through parser
+let line1 = []
+let line2 = []
+let line3 = []
+let line4 = []
 rl.on('line', (line) => {
   // TODO: use counter to split into lines of four
   // restart new array every time counter resets
@@ -13,10 +21,11 @@ rl.on('line', (line) => {
   
   console.log(`Line from file: ${line}`);
   if(counter < 4){
-    // send line into parse function in array
+    // set lines 1-4 here as iteration happens
     counter += 1
   } else {
-    // reset to start building new array
+    // send lines 1-4 to parse function and create account number
+    // reset lines 1-4 and start over
     counter = 1
   }
   
