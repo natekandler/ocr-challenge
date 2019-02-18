1df8d5ac274cadb4d195f02be395f10be45040cb
natekandler <natekandler@gmail.com>
2019-02-17 16:46:02 -0800

    adds counter to file reader to break into fours

--- src/FileReader.js
+++ src/FileReader.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 const readline = require('readline');
 
-async function processLineByLine() {
-  const fileStream = fs.createReadStream('Accounts.txt');
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  for await (const line of rl) {
-    console.log(`Line from file: ${line}`);
+const rl = readline.createInterface({
+  input: fs.createReadStream('Accounts.txt'),
+  crlfDelay: Infinity
+});
+let counter = 1
+rl.on('line', (line) => {
+  // TODO: use counter to split into lines of four
+  // restart new array every time counter resets
+  console.log(counter)
+  
+  console.log(`Line from file: ${line}`);
+  if(counter < 4){
+    // send line into parse function in array
+    counter += 1
+  } else {
+    // reset to start building new array
+    counter = 1
   }
-}
-
-processLineByLine();
+  
+});
